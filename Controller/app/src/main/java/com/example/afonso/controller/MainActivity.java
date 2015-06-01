499009e9c09d11f86b0612175ed9c8b08ea0eaf3
afonso <afonso..castro9@yahoo.com>
2015-06-02 00:43:23 +0100

    Controlador 100% feito

--- Controller/app/src/main/java/com/example/afonso/controller/MainActivity.java
+++ Controller/app/src/main/java/com/example/afonso/controller/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.afonso.controller;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import java.io.DataOutputStream;
@@ -14,46 +19,97 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class MainActivity extends Activity {
-    private short analogXAxis = 0;
-    private short analogYAxis = 0;
     private float endX = 0;
     private float endY = 0;
+
+    private short analogXAxis = 0;
+    private short analogYAxis = 0;
     private boolean actButton = false;
+
     private String hostName = "192.168.43.60";
-    private int port = 25293;
+    private int port = 25294;
     private Socket socket;
     private DataOutputStream data;
+    boolean connected = false;
+
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Thread t = new Thread(new Runnable(){
-            public void run() {
-                connect();
-            }
-        });
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         setContentView(R.layout.controller);
         ImageButton actionButton = (ImageButton) findViewById(R.id.actionButton);
         actionButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Log.d("PC", "Foi a true");
-                    actButton = true;
-                    sendInfo();
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    Log.d("PC", "Foi a false");
-                    actButton = false;
-                    sendInfo();
+                if(connected) {
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        Log.d("PC", "Foi a true");
+                        actButton = true;
+                        sendInfo();
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        Log.d("PC", "Foi a false");
+                        actButton = false;
+                        sendInfo();
+                    } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    }
                 }
-                else if(event.getAction() == MotionEvent.ACTION_MOVE){}
                 return true;
+            }
+        });
+
+        final ImageButton connect = (ImageButton)findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.popup, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.edittext);
+
+                // set dialog message
+                alertDialogBuilder.setTitle("Connect to server")
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        hostName = userInput.getText().toString();
+                                        Thread t = new Thread(new Runnable(){
+                                            public void run() {
+                                                connect();
+                                            }
+                                        });
+                                        t.start();
+                                        try {
+                                            t.join();
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
             }
         });
 
@@ -61,32 +117,34 @@ public class MainActivity extends Activity {
         analogStick.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                endX = 2 * analogStick.getPivotX();
-                endY = 2 * analogStick.getPivotY();
-                if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-                    analogXAxis = (short) ((event.getX() * 255) / endX);
-                    analogYAxis = (short) ((event.getY() * 255) / endY);
+                if (connected) {
+                    endX = 2 * analogStick.getPivotX();
+                    endY = 2 * analogStick.getPivotY();
+                    if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
+                        analogXAxis = (short) ((event.getX() * 255) / endX);
+                        analogYAxis = (short) ((event.getY() * 255) / endY);
 
-                    if (event.getX() > 255) {
-                        analogXAxis = 255;
-                    } else if(event.getX() < 0){
-                        analogXAxis = 0;
-                    }
+                        if (event.getX() > 255) {
+                            analogXAxis = 255;
+                        } else if (event.getX() < 0) {
+                            analogXAxis = 0;
+                        }
 
-                    if (event.getY() > 255) {
-                        analogYAxis = 255;
-                    } else if(event.getY() < 0){
-                        analogYAxis = 0;
+                        if (event.getY() > 255) {
+                            analogYAxis = 255;
+                        } else if (event.getY() < 0) {
+                            analogYAxis = 0;
+                        }
+                        Log.v("touchd x val of cap img", analogXAxis + "");
+                        Log.v("touchd y val of cap img", analogYAxis + "");
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        analogXAxis = 128;
+                        analogYAxis = 128;
+                        Log.v("touchd x val of cap img", analogXAxis + "");
+                        Log.v("touchd y val of cap img", analogYAxis + "");
                     }
-                    Log.v("touchd x val of cap img", analogXAxis + "");
-                    Log.v("touchd y val of cap img", analogYAxis + "");
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    analogXAxis = 0;
-                    analogYAxis = 0;
-                    Log.v("touchd x val of cap img", 0 + "");
-                    Log.v("touchd y val of cap img", 0 + "");
+                    sendInfo();
                 }
-                sendInfo();
                 return true;
             }
         });
@@ -95,10 +153,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(connected) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -117,6 +177,7 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        connected = true;
     }
 
     protected void sendInfo(){
